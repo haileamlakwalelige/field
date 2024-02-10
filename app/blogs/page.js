4a61b81844b87d38a798ff3a02d95caad781edd7
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-02-11 00:34:00 +0300

    add blog

---
+++ app/blogs/page.js
@@ -0,0 +1,12 @@
+import React from "react";
+import BlogIntro from "../components/blogs/BlogIntro";
+
+const Blogs = () => {
+  return (
+    <div>
+      <BlogIntro />
+    </div>
+  );
+};
+
+export default Blogs;

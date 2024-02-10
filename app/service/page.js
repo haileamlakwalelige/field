966c0a7b514cad5d2d2ac209310796aabdcc9aa6
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-02-11 00:35:10 +0300

    add service

---
+++ app/service/page.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ServiceWhole from '../components/service/ServiceWhole';
+import HomeReady from '../components/home/HomeReady';
+
+const Service = () => {
+  return (
+    <div>
+        <ServiceWhole />
+        <HomeReady />
+    </div>
+  )
+}
+
+export default Service;

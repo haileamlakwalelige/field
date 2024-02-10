b5124385fecd6819a1b662c5c931698233f637c2
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-02-11 00:48:59 +0300

    adding style to the service section

--- app/service/page.js
+++ app/service/page.js
@@ -1,14 +1,14 @@
-import React from 'react'
-import ServiceWhole from '../components/service/ServiceWhole';
-import HomeReady from '../components/home/HomeReady';
+import React from "react";
+import ServiceWhole from "../components/service/ServiceWhole";
+import HomeReady from "../components/home/HomeReady";
 
 const Service = () => {
   return (
-    <div>
-        <ServiceWhole />
-        <HomeReady />
+    <div className="mt-20">
+      <ServiceWhole />
+      <HomeReady />
     </div>
-  )
-}
+  );
+};
 
 export default Service;

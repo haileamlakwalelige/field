a1277c24d98b087bf40418ec3fa7c1a9a00d0dc6
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-02-11 03:32:41 +0300

    update sidebar and add popular books

--- app/components/Sidebar.jsx
+++ app/components/Sidebar.jsx
@@ -2,6 +2,8 @@
 
 import React, { useState } from "react";
 import { FaChevronDown, FaChevronUp } from "react-icons/fa6";
+import { FaTimesCircle } from "react-icons/fa";
+import { AiOutlineBars } from "react-icons/ai";
 
 const Sidebar = () => {
   const [show, setShow] = useState(1);
@@ -19,41 +21,54 @@ const Sidebar = () => {
     <div
       className={
         width
-          ? "min-w-[300px] max-w-[350px] border-2 border-gray-200 shadow-xl rounded px-5 justify-center mr-5 py-5"
-          : "w-[200px] border-2 border-gray-200 shadow-xl rounded px-5 justify-center py-5"
+          ? "min-w-[350px] max-w-[400px] border-2 border-gray-200 shadow-xl rounded px-5 justify-center mr-5 py-5"
+          : "w-[330px] border-2 mr-5 border-gray-200 shadow-xl rounded px-5 justify-center py-5"
       }
     >
-      <p onClick={handleWidth} className="cursor-pointer flex justify-end">
-        +
-      </p>
+      <div
+        onClick={handleWidth}
+        className="cursor-pointer flex justify-between mb-5"
+      >
+        <p className="font-semibold text-[20px]">Applied Filters</p>
+        {width ? <FaTimesCircle size={20} /> : <AiOutlineBars size={20} />}
+      </div>
+      <p className="py-2 cursor-pointer text-[20px] font-medium">Book Type</p>
       <div>
         <div>
           <div onClick={() => handleShow(1)}>
-            <div className="flex justify-between items-center">
-              <p className="py-2 cursor-pointer text-[18px]">Antiques & Collectibles</p>
-              {show === 1 ? <FaChevronDown /> : <FaChevronUp />}
+            <div className="flex justify-between items-center cursor-pointer">
+              <p className="py-2 cursor-pointer text-[18px] font-medium">
+                Antiques & Collectibles
+              </p>
+              {width && (show === 1 ? <FaChevronDown /> : <FaChevronUp />)}
             </div>
             {show === 1 && (
               <div className="flex flex-col gap-3 px-4 mt-3 justify-center items-start">
-                <p className="cursor-pointer">Autographs</p>
-                <p className="cursor-pointer">Buttons & Pins</p>
-                <p className="cursor-pointer">Coins, Currency & Medals</p>
-                <p className="cursor-pointer">Jewelry</p>
-                <p className="cursor-pointer">Magazines & Newspapers</p>
-                <p className="cursor-pointer">Military</p>
-                <p className="cursor-pointer">Paper Ephemera</p>
-                <p className="cursor-pointer">Popular Culture</p>
-                <p className="cursor-pointer">Posters</p>
-                <p className="cursor-pointer">Pottery & Ceramics</p>
-                <p className="cursor-pointer">Textiles & Costume</p>
+                <p className="cursor-pointer text-[16px]">Autographs</p>
+                <p className="cursor-pointer text-[16px]">Buttons & Pins</p>
+                <p className="cursor-pointer text-[16px]">
+                  Coins, Currency & Medals
+                </p>
+                <p className="cursor-pointer text-[16px]">Jewelry</p>
+                <p className="cursor-pointer text-[16px]">
+                  Magazines & Newspapers
+                </p>
+                <p className="cursor-pointer text-[16px]">Military</p>
+                <p className="cursor-pointer text-[16px]">Paper Ephemera</p>
+                <p className="cursor-pointer text-[16px]">Popular Culture</p>
+                <p className="cursor-pointer text-[16px]">Posters</p>
+                <p className="cursor-pointer text-[16px]">Pottery & Ceramics</p>
+                <p className="cursor-pointer text-[16px]">Textiles & Costume</p>
               </div>
             )}
           </div>
 
           <div onClick={() => handleShow(2)}>
-            <div className="flex justify-between items-center">
-              <p className="py-2 cursor-pointer ">Books1</p>
-              {show === 1 ? <FaChevronDown /> : <FaChevronUp />}
+            <div className="flex justify-between items-center cursor-pointer">
+              <p className="py-2 cursor-pointer pt-4 font-medium text-[18px]">
+                Books2
+              </p>
+              {width && (show === 2 ? <FaChevronDown /> : <FaChevronUp />)}
             </div>
             {show === 2 && (
               <div className="flex flex-col gap-3 px-2 mt-3 justify-center items-start">
--- app/components/books/BooksAll.jsx
+++ app/components/books/BooksAll.jsx
@@ -1,9 +1,67 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
 const BooksAll = () => {
   return (
-    <div>BooksAll</div>
-  )
-}
+    <div>
+      <div>
+        <p className="text-black text-[32px] font-semibold roboto py-3">
+          Popular Books
+        </p>
+        <div className="carousel gap-10">
+          <div className="carousel-item rounded">
+            <img
+              src="https://daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.jpg"
+              alt="Burger"
+              className="w-[200px] h-[250px]"
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              src="https://daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.jpg"
+              alt="Burger"
+              className="w-[200px] h-[250px]"
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              src="https://daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.jpg"
+              alt="Burger"
+              className="w-[200px] h-[250px]"
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              src="https://daisyui.com/images/stock/photo-1494253109108-2e30c049369b.jpg"
+              alt="Burger"
+              className="w-[200px] h-[250px]"
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              src="https://daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.jpg"
+              alt="Burger"
+              className="w-[200px] h-[250px]"
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              src="https://daisyui.com/images/stock/photo-1559181567-c3190ca9959b.jpg"
+              alt="Burger"
+              className="w-[200px] h-[250px]"
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              src="https://daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.jpg"
+              alt="Burger"
+              className="w-[200px] h-[250px]"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default BooksAll
+export default BooksAll;

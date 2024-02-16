6d81c1ef99dc10cfc1a89a7e90738b28cb13f78b
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-02-16 14:46:38 -0800

    start login page

---
+++ app/components/login/LoginIntro.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LoginIntro = () => {
+  return (
+    <div>LoginIntro</div>
+  )
+}
+
+export default LoginIntro
---
+++ app/login/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import LoginIntro from '../components/login/LoginIntro';
+
+const Login = () => {
+  return (
+    <div>
+        <LoginIntro />
+    </div>
+  )
+}
+
+export default Login;

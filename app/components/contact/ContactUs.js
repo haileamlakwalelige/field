1a7d1147e0d0c45853e07c2fe4989cfb4fb925f4
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-02-16 14:34:00 -0800

    add background

--- app/components/contact/ContactUs.js
+++ app/components/contact/ContactUs.js
@@ -7,7 +7,7 @@ import { LuPhone } from "react-icons/lu";
 
 const ContactUs = () => {
   return (
-    <div className='px-2 sm:px-6 md:px-12 lg:px-20 mt-10 justify-center items-center'>
+    <div style={{ backgroundImage: `url(${bgs})` }} className='contact px-2 sm:px-6 md:px-12 lg:px-20 mt-10 justify-center items-center'>
       <div className='bg-[#846640] lg:min-h-[500px] grid grid-cols-1 lg:grid-cols-2  justify-center items-center lg:w-full overflow-hidden'>
       <div className='bg-[#846640] lg:min-h-[500px] flex flex-col text-white roboto'>
         <p className='text-white font-semibold roboto pt-5 text-[20px] md:text-[30px] lg:text-[40px] text-center pb-4 sm:pb-8 md:pb-12 '>Let&apos; Get in Touch</p>

0aeb3053a326b64d618f7ba6546feb820929795d
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-02-16 14:34:28 -0800

    remove flowbite

--- app/layout.js
+++ app/layout.js
@@ -10,7 +10,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>{children}</body>
-      <script src="../path/to/flowbite/dist/flowbite.min.js"></script>
     </html>
   );
 }

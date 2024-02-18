8489a24f66803b95657248fcab6c61756e8da35e
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-02-18 07:38:41 -0800

    update the home intro section

--- app/components/home/HomeTry.jsx
+++ app/components/home/HomeTry.jsx
@@ -1,34 +1,33 @@
 "use client"
 
-
-
 import React, { useState, useEffect } from 'react';
 import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs';
 import { RxDotFilled } from 'react-icons/rx';
 
 function HomeIntro() {
-  const slides = [
-    {
-      url: 'https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2620&q=80',
-      text: 'Use Client',
-    },
-    {
-      url: 'https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80',
-      text: 'Use Client 2',
-    },
-    {
-      url: 'https://images.unsplash.com/photo-1661961112951-f2bfd1f253ce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2672&q=80',
-      text: 'Use Client 3',
-    },
-    {
-      url: 'https://images.unsplash.com/photo-1512756290469-ec264b7fbf87?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2253&q=80',
-      text: 'Use Client 4',
-    },
-    {
-      url: 'https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2671&q=80',
-      text: 'Use Client 5',
-    },
-  ];
+    const slides = [
+        {
+          url: 'https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2620&q=80',
+          text: 'Welcome to QuantumField: Bringing into existence out of nothing!',
+          description:"",
+        },
+        {
+          url: 'https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80',
+          text: 'Use Client 2',
+        },
+        {
+          url: 'https://images.unsplash.com/photo-1661961112951-f2bfd1f253ce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2672&q=80',
+          text: 'Use Client 3',
+        },
+        {
+          url: 'https://images.unsplash.com/photo-1512756290469-ec264b7fbf87?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2253&q=80',
+          text: 'Use Client 4',
+        },
+        {
+          url: 'https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2671&q=80',
+          text: 'Use Client 4',
+        },
+      ];
 
   const [currentIndex, setCurrentIndex] = useState(0);
 
@@ -60,9 +59,11 @@ function HomeIntro() {
     <div className='max-w-screen h-[90vh] w-full top-0 -mt-10 relative group'>
       <div
         style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
-        className='w-full h-full  bg-center bg-cover duration-500'
+        className='w-full h-full bg-center bg-cover duration-500 relative'
       >
-       
+        <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-[30px] md:text-[40px]  lg:text-[45px] font-bold cri text-center'>
+          {slides[currentIndex].text}
+        </div>
       </div>
       {/* Left Arrow */}
       <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
@@ -77,7 +78,7 @@ function HomeIntro() {
           <div
             key={slideIndex}
             onClick={() => goToSlide(slideIndex)}
-            className='text-7xl text-white  -mt-20 rounded-full cursor-pointer mx-3 font-bold h-[10px] w-[10px]'
+            className='text-7xl text-black  -mt-32 rounded-full cursor-pointer mx-3 font-bold h-[10px] w-[10px]'
           >
             <RxDotFilled />
           </div>

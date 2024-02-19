f722e6ac7f8923381bd6e9431a01f1768edeb379
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-02-18 21:35:04 -0800

    update link

--- app/components/layout/Navbar.jsx
+++ app/components/layout/Navbar.jsx
@@ -57,7 +57,7 @@ const Navbar = () => {
           <li><a href="/">Home</a></li>
           <li><a href="/service">Services</a></li>
           <li><a href="/books">Books</a></li>
-          <li><a href="/blog">Blog</a></li>
+          <li><a href="/blogs">Blog</a></li>
           <li><a href="/contact">Contact</a></li>
         </ul>
       </div>

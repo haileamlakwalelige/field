b7e8be595723da6aa073c70f6cd8403f5a34b2d5
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-02-20 02:22:03 +0300

    start activation page

---
+++ app/activation/page.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Activation = () => {
+  return (
+    <div className="mt-32">
+      <div className="bg-[#F2D050] min-[350px] max-w-[700px] flex flex-col justify-center items-center">
+        <p className="font-semibold text-[20px] md:text-[22px] lg:text-[24px] ">
+          Thank you for buying our book. your activation key:
+        </p>
+        <p className="font-bold text-[20px] md:text-[22px] lg:text-[24px] ">
+          qcyLtO0DkU
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Activation;

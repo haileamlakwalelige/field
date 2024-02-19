5c5487d3a27d31dc3cdcaee800c31377b34138e3
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-02-20 02:24:44 +0300

    add activation card

--- app/activation/page.js
+++ app/activation/page.js
@@ -2,10 +2,10 @@ import React from "react";
 
 const Activation = () => {
   return (
-    <div className="mt-32">
-      <div className="bg-[#F2D050] min-[350px] max-w-[700px] flex flex-col justify-center items-center">
-        <p className="font-semibold text-[20px] md:text-[22px] lg:text-[24px] ">
-          Thank you for buying our book. your activation key:
+    <div className="mt-32 flex flex-col justify-center items-center">
+      <div className="bg-[#F2D050] min-h-[350px] max-w-[700px] lg:min-w-[700px] flex flex-col justify-center items-center">
+        <p className="text-center text-[20px] md:text-[22px] lg:text-[24px]  px-2 sm:px-4 md:px-8 lg:px-16">
+          Thank you for buying our book. <br /> your activation key:
         </p>
         <p className="font-bold text-[20px] md:text-[22px] lg:text-[24px] ">
           qcyLtO0DkU

cfe9e35bd3d1adf9db2c514cc15d7dbb5c7baee7
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-02-26 02:31:30 +0300

    create slice

---
+++ app/store/cartSlice.js
@@ -0,0 +1,21 @@
+"use client"
+
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState=[];
+
+
+const cartSlice=createSlice({
+  name:'cart',
+  initialState,
+  reducers:{
+    add(state, action){
+      state.push(action.payload)
+    }
+  }
+});
+
+
+
+export const {add} = cartSlice.actions;
+export default cartSlice.reducer;

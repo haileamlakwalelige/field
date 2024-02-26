a145cf06178d4e2d0780529fafeb03e3dfcb5a5c
haileamlakwalelige <haileamlakwaleligne3910@gmail.com>
2024-02-26 03:19:57 +0300

    create cart page

---
+++ app/cart/page.js
@@ -0,0 +1,15 @@
+"use client"
+
+
+import React from "react";
+import CartAll from "../components/cart/CartAll";
+
+const Cart = () => {
+  return (
+    <div className="mt-32">
+      <CartAll />
+    </div>
+  );
+};
+
+export default Cart;
